d2bc8243dabc251f2cbd787e7eb8f36b0449789d
mmi01 <zmi1@sheffield.ac.uk>
2023-08-03 02:41:49 +0100

    added script for training LD model

---
+++ train/pretrain-ld-gpt-small-fi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export HF_HOME=../../.cache
+
+LD_LIBRARY_PATH=""
+python pretrain-tc-gpt-small-fi.py \
+--tc-scheme ld \
+--output-dir ../../cl-ld-gpt-small-fi \
+--max-positions 512 \
+--embed-dimension 768 \
+--layers 12 \
+--attention-heads 12 \
+--lr 3e-5 \
+--bsz 64 \

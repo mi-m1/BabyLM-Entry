99ae856c770645eac7b78daaeee29207cfbb54b6
mmi01 <zmi1@sheffield.ac.uk>
2023-08-03 01:05:55 +0100

    Added script for generating the files required for textcomplexity to run

---
+++ process-data/generate_conll-x.sh
@@ -0,0 +1,12 @@
+# save this file in textcomplexity/utils/
+
+python run_stanza.py -l en -o /mnt/parscratch/users/acq22zm/babylm-challenge/process-data/tc/ /mnt/parscratch/users/acq22zm/babylm-challenge/data/babylm_data/babylm_10M/aochildes.train
+python run_stanza.py -l en -o /mnt/parscratch/users/acq22zm/babylm-challenge/process-data/tc/ /mnt/parscratch/users/acq22zm/babylm-challenge/data/babylm_data/babylm_10M/bnc_spoken.train
+python run_stanza.py -l en -o /mnt/parscratch/users/acq22zm/babylm-challenge/process-data/tc/ /mnt/parscratch/users/acq22zm/babylm-challenge/data/babylm_data/babylm_10M/cbt.train
+python run_stanza.py -l en -o /mnt/parscratch/users/acq22zm/babylm-challenge/process-data/tc/ /mnt/parscratch/users/acq22zm/babylm-challenge/data/babylm_data/babylm_10M/children_stories.train
+python run_stanza.py -l en -o /mnt/parscratch/users/acq22zm/babylm-challenge/process-data/tc/ /mnt/parscratch/users/acq22zm/babylm-challenge/data/babylm_data/babylm_10M/gutenberg.train
+python run_stanza.py -l en -o /mnt/parscratch/users/acq22zm/babylm-challenge/process-data/tc/ /mnt/parscratch/users/acq22zm/babylm-challenge/data/babylm_data/babylm_10M/open_subtitles.train
+python run_stanza.py -l en -o /mnt/parscratch/users/acq22zm/babylm-challenge/process-data/tc/ /mnt/parscratch/users/acq22zm/babylm-challenge/data/babylm_data/babylm_10M/qed.train
+python run_stanza.py -l en -o /mnt/parscratch/users/acq22zm/babylm-challenge/process-data/tc/ /mnt/parscratch/users/acq22zm/babylm-challenge/data/babylm_data/babylm_10M/simple_wikipedia.train
+python run_stanza.py -l en -o /mnt/parscratch/users/acq22zm/babylm-challenge/process-data/tc/ /mnt/parscratch/users/acq22zm/babylm-challenge/data/babylm_data/babylm_10M/switchboard.train
+python run_stanza.py -l en -o /mnt/parscratch/users/acq22zm/babylm-challenge/process-data/tc/ /mnt/parscratch/users/acq22zm/babylm-challenge/data/babylm_data/babylm_10M/wikipedia.train
